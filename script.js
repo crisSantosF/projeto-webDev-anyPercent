63a4e0bc0a6146931e11febc76a959d563783c4f
Cristopher dos Santos <crissfilho@gmail.com>
2025-09-07 02:29:57 -0300

    primeiras telas completas e funcionais

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
-//***PAGINA PRINCIPAL*** //
+////////////////////***PRIMEIRA TELA*** ////////////////////////
 
 const palavra_rotatoria = document.getElementById('cycling_word');
 const palavras_para_rotacionar = ['mensal', 'semanal', 'diário'];
 const animacao_atual = ['aparece', 'desaparece'];
+
 let index_palavras_para_rotacionar = 0;
+let trava_rotacaoPalavras = 0;
 
 
 function iniciar_rotacao() 
@@ -29,24 +31,45 @@ function agendar_proxRotacao()
     palavra_rotatoria.classList.remove('rotacao-aparece');
     
     // Usa setTimeout para criar a pausa de 2 segundos antes de reiniciar o ciclo
-    setTimeout(iniciar_rotacao, 2000);
+    trava_rotacaoPalavras = setTimeout(iniciar_rotacao, 2000);
 }
 
-
-palavra_rotatoria.addEventListener('animationend', (event) =>
+function gerente_trocaPalavra_ou_rotacaoPalavra(event)
 {
-
     if (event.animationName === 'rotacao_desaparece') {
         trocar_palavra_e_aparecer();
     } else if (event.animationName === 'rotacao_aparece') {
         agendar_proxRotacao();
     }
-});
+};
+
+palavra_rotatoria.addEventListener('animationend', gerente_trocaPalavra_ou_rotacaoPalavra);
 
 // Inicia o primeiro ciclo de todos.
-setTimeout(iniciar_rotacao, 2000);
+trava_rotacaoPalavras = setTimeout(iniciar_rotacao, 2000);
 
 
+////////////////////***SEGUNDA TELA*** ////////////////////////
+
+
+const botao_transicao = document.getElementById('botao-transicao-primeiraTela-segundaTela');
+const primeira_tela = document.getElementById('conteiner-telaPrincipal');
+const segunda_tela = document.getElementById('segunda-tela_aux');
+
+botao_transicao.addEventListener('click', () =>
+{
+    primeira_tela.classList.add('animacao-esconde-tela');
+
+    primeira_tela.addEventListener('animationend', () =>
+    {
+        segunda_tela.classList.remove('esconde-tela');
+        primeira_tela.classList.add('esconde-tela');
+
+        clearTimeout(trava_rotacaoPalavras);
+        palavra_rotatoria.removeEventListener('animationend', gerente_trocaPalavra_ou_rotacaoPalavra);
+
+    }, {once:true})
+}, {once:true})
 
 
 
