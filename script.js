64840c68529c8559f7803456553d1e52004ec1a9
Cristopher dos Santos <crissfilho@gmail.com>
2025-09-09 12:18:16 -0300

    todas as telas integradas

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
+//////////////////** MÉTODOS PARA CAPTURA DA DATA ATUAL **////////////////
+const cincoHoras = 5000000;
+
+const dataAtual = new Date();
+const primeiroDia_semana = new Date(dataAtual);
+
+//getDate é o dia do mes (1-31) e getDay é o dia da semana (0-6)
+//portanto getDate - getDay = primeiro dia da semana e o + 1 é pois, por padrão,
+//o prieiro dia do objeto date é domingo
+
+//setDate lida com as possiveis complicações de, por exemplo , primeiroDia + 6 for maior
+// que o ultimo dia do mes do primeiro dia (28 de fevereiro + 6, por exemplo)
+// mas ele retorna um giga inteiro que conts milissegundos e ele altera os objetos aos quais
+//ele é aplicadp, então deve se criar cópias para não dar confusão
+primeiroDia_semana.setDate(dataAtual.getDate() - dataAtual.getDay() + 1);
+
+//nesse caso eu criei uma cópia do primeiro new Date() e depois criei uma outra cópia,
+//mas do primeiroDia para lidar com o setDate de acordo, filtrando as coplicações de 
+// ultimoDia > 31, por exemplo.
+const ultimoDia_semana = new Date(primeiroDia_semana);
+ultimoDia_semana.setDate(primeiroDia_semana.getDate() + 6);
+
+const primeiroDia_filtrado = primeiroDia_semana.getDate();
+const ultimoDia_filtrado = ultimoDia_semana.getDate();
+
+// getMonth() + 1 pois getMonth() é 0indexed, então setembro, 9, acaba sendo 8
+const mes_primeiroDia = primeiroDia_semana.getMonth() + 1;
+const mes_ultimoDia = ultimoDia_semana.getMonth() + 1;
+
+
+
+
+
 ////////////////////***PRIMEIRA TELA*** ////////////////////////
-/*
+
 const palavra_rotatoria = document.getElementById('cycling_word');
 const palavras_para_rotacionar = ['mensal', 'semanal', 'diário'];
 const animacao_atual = ['aparece', 'desaparece'];
@@ -15,7 +48,6 @@ function iniciar_rotacao()
 
 function trocar_palavra_e_aparecer() 
 {
-  
     palavra_rotatoria.classList.remove('rotacao-desaparece');
 
     index_palavras_para_rotacionar = (index_palavras_para_rotacionar + 1) % palavras_para_rotacionar.length;
@@ -55,7 +87,7 @@ const botao_transicao = document.getElementById('botao-transicao-primeiraTela-se
 const primeira_tela = document.getElementById('conteiner-telaPrincipal');
 const segunda_tela = document.getElementById('segunda-tela_aux');
 
-function espera_transicaoTelas()
+function espera_transicao_primeiraSegunda_telas()
 {    
     segunda_tela.classList.remove('esconde-tela');
     primeira_tela.classList.add('esconde-tela');
@@ -64,93 +96,99 @@ function espera_transicaoTelas()
     palavra_rotatoria.removeEventListener('animationend', gerente_trocaPalavra_ou_rotacaoPalavra);
 }
 
-function botaoTransicao_clicado()
+function botaoTransicao_para_segundaTela()
 {
     primeira_tela.classList.add('animacao-esconde-tela');
 
-    primeira_tela.addEventListener('animationend', espera_transicaoTelas())
+    primeira_tela.addEventListener('animationend', espera_transicao_primeiraSegunda_telas)
 }
 
 
-botao_transicao.addEventListener('click', botaoTransicao_clicado);
+botao_transicao.addEventListener('click', botaoTransicao_para_segundaTela);
 
-primeira_tela.removeEventListener('animationend', espera_transicaoTelas);
-botao_transicao.removeEventListener('clicked', botaoTransicao_clicado);
 
-*/
+//remoção de event listeners que não serão mais usados
+primeira_tela.removeEventListener('animationend', espera_transicao_primeiraSegunda_telas);
+botao_transicao.removeEventListener('clicked', botaoTransicao_para_segundaTela);
+
+
+// transicao para tela de tarefas
+const telaTarefas = document.getElementById('telaTarefas_aux');
+
+function transicao_para_telaTarefas()
+{
+    segunda_tela.classList.add('esconde-tela');
+
+    telaTarefas.classList.remove('esconde-tela');
+}
+
+function botao_transicao_para_telaTarefas(event)
+{
+    const escopo_de_escolha = event.target;
+
+    if(escopo_de_escolha.textContent.trim().toLowerCase() === 'mensal')
+    {
+        const escopoAtual = document.getElementById('escopoAtual');
+
+        const novoEscopo = `<div id="escopoAtual">
+                                <select id="escolha_mes" class="mb" name="mes_de_escolha">
+                                    <option class="titulo">Janeiro</option>
+                                    <option class="titulo">Fevereiro</option>
+                                    <option class="titulo">Março</option>
+                                    <option class="titulo">Abril</option>
+                                    <option class="titulo">Maio</option>
+                                    <option class="titulo">Junho</option>
+                                    <option class="titulo">Julho</option>
+                                    <option class="titulo">Agosto</option>
+                                    <option class="titulo">Setembro</option>
+                                    <option class="titulo">Outubro</option>
+                                    <option class="titulo">Novembro</option>
+                                    <option class="titulo">Dezembro</option>
+                                </select>
+                            </div>`
+                            
+
+        escopoAtual.outerHTML = novoEscopo;
+
+        const seletorMes = document.getElementById('escolha_mes');
+        seletorMes.selectedIndex = mes_primeiroDia - 1;
+    }
+    else if(escopo_de_escolha.textContent.trim().toLowerCase() === 'semanal')
+    {
+        const escopoAtual = document.getElementById('escopoAtual');
+
+        const novoEscopo = document.createElement('div');
+        novoEscopo.id = 'escopoAtual';
+        novoEscopo.textContent = `${primeiroDia_filtrado}/${mes_primeiroDia} - ${ultimoDia_filtrado}/${mes_ultimoDia}`;
+    
+        escopoAtual.replaceWith(novoEscopo);
+    }
+    else
+    {
+        const escopoAtual = document.getElementById('escopoAtual');
+
+        const novoEscopo = document.createElement('div');
+        novoEscopo.id = 'escopoAtual';
+        novoEscopo.textContent = `${primeiroDia_filtrado}/${mes_primeiroDia}`;
+     
+        escopoAtual.replaceWith(novoEscopo);   
+    }
+
+
+    segunda_tela.classList.add('animacao-esconde-tela');
+
+    segunda_tela.addEventListener('animationend', transicao_para_telaTarefas);
+}
+
+const verifica_botaoClicado = document.getElementById('tela-selecao-escopoDePlanejamento')
+
+verifica_botaoClicado.addEventListener('click', botao_transicao_para_telaTarefas);
+
+
+
+
 
 ////////////////////***TELA TAREFAS*** ////////////////////////
-
-
-//adição de tarefas
-const botao_adicionaTarefa = document.getElementById('botao_adicionaTarefa');
-const conteiner_tarefas = document.getElementById('tarefas');
-
-function cria_novaTarefa(textoTarefa)
-{
-    const conteiner_tarefaEspecifica = document.createElement('div');
-    conteiner_tarefaEspecifica.classList.add('tarefaEspecifica');
-
-    const conteiner_informacaoTarefa = document.createElement('div');
-    conteiner_informacaoTarefa.classList.add('informacaoTarefa');
-
-    const checagemTarefa = document.createElement('span');
-    checagemTarefa.classList.add('material-symbols-outlined', 'checkbox');
-    checagemTarefa.textContent = 'radio_button_unchecked';
-
-    const paragrafoTarefa = document.createElement('p');
-    paragrafoTarefa.textContent = textoTarefa;
-
-    conteiner_informacaoTarefa.appendChild(checagemTarefa);
-    conteiner_informacaoTarefa.appendChild(paragrafoTarefa);
-
-    conteiner_tarefaEspecifica.appendChild(conteiner_informacaoTarefa);
-
-    conteiner_tarefas.appendChild(conteiner_tarefaEspecifica);
-}
-
-function botao_novaTarefa_clicado()
-{
-    const textoTarefa = prompt('o que vamos fazer?');
-
-    if(textoTarefa && textoTarefa.trim() !== '')
-        cria_novaTarefa(textoTarefa);
-}
-
-botao_adicionaTarefa.addEventListener('click', botao_novaTarefa_clicado);
-
-//checagem de tarefas
-
-function tarefaChecada(event)
-{
-    if(event.target.classList.contains('checkbox'))
-    {
-        const checkBox_clicado = event.target;
-        const informacaoTarefa = checkBox_clicado.parentElement;
-        const textoTarefa = informacaoTarefa.querySelector('p');
-
-        //essa estrutura é meio nojenta, mas serve pra atribuir um booleano caso, nesse exemplo,
-        //o texto do elemento checkbox_clicado for igual a 'radio_button_checked'
-        const foiChecada = checkBox_clicado.textContent === 'radio_button_checked';
-
-        if(foiChecada)
-        {
-            checkBox_clicado.textContent = 'radio_button_unchecked';
-            checkBox_clicado.classList.remove('checked');
-            textoTarefa.classList.remove('tarefaConcluida');
-        }
-        else
-        {
-            checkBox_clicado.textContent = 'radio_button_checked';
-            checkBox_clicado.classList.add('checked');
-            textoTarefa.classList.add('tarefaConcluida');
-        }
-    }
-}
-
-
-conteiner_tarefas.addEventListener('click', tarefaChecada);
-
 
 //troca de escopo
 
@@ -158,32 +196,6 @@ const botao_trocaEscopo = document.getElementById('trocaEscopo');
 
 function trocaEscopo(event)
 {
-    const dataAtual = new Date();
-    const primeiroDia_semana = new Date(dataAtual);
-
-    //getDate é o dia do mes (1-31) e getDay é o dia da semana (0-6)
-    //portanto getDate - getDay = primeiro dia da semana e o + 1 é pois, por padrão,
-    //o prieiro dia do objeto date é domingo
-
-    //setDate lida com as possiveis complicações de, por exemplo , primeiroDia + 6 for maior
-    // que o ultimo dia do mes do primeiro dia (28 de fevereiro + 6, por exemplo)
-    // mas ele retorna um giga inteiro que conts milissegundos e ele altera os objetos aos quais
-    //ele é aplicadp, então deve se criar cópias para não dar confusão
-    primeiroDia_semana.setDate(dataAtual.getDate() - dataAtual.getDay() + 1);
-
-    //nesse caso eu criei uma cópia do primeiro new Date() e depois criei uma outra cópia,
-    //mas do primeiroDia para lidar com o setDate de acordo, filtrando as coplicações de 
-    // ultimoDia > 31, por exemplo.
-    const ultimoDia_semana = new Date(primeiroDia_semana);
-    ultimoDia_semana.setDate(primeiroDia_semana.getDate() + 6);
-
-    const primeiroDia_filtrado = primeiroDia_semana.getDate();
-    const ultimoDia_filtrado = ultimoDia_semana.getDate();
-
-    // getMonth() + 1 pois getMonth() é 0indexed, então setembro, 9, acaba sendo 8
-    const mes_primeiroDia = primeiroDia_semana.getMonth() + 1;
-    const mes_ultimoDia = ultimoDia_semana.getMonth() + 1;
-
     const escopo_de_escolha = event.target;
 
     if(escopo_de_escolha.textContent.trim().toLowerCase() === 'semanal')
@@ -195,8 +207,6 @@ function trocaEscopo(event)
         novoEscopo.textContent = `${primeiroDia_filtrado}/${mes_primeiroDia} - ${ultimoDia_filtrado}/${mes_ultimoDia}`;
     
         escopoAtual.replaceWith(novoEscopo);
-
-
     }
     else if(escopo_de_escolha.textContent.trim().toLowerCase() === 'diário')
     {
@@ -238,3 +248,106 @@ function trocaEscopo(event)
 }
 
 botao_trocaEscopo.addEventListener('click', trocaEscopo);
+
+
+
+//adição de tarefas
+
+
+const botao_adicionaTarefa = document.getElementById('botao_adicionaTarefa');
+const conteiner_tarefas = document.getElementById('tarefas');
+
+function cria_novaTarefa(textoTarefa)
+{
+    const conteiner_tarefaEspecifica = document.createElement('div');
+    conteiner_tarefaEspecifica.classList.add('tarefaEspecifica');
+
+    const conteiner_informacaoTarefa = document.createElement('div');
+    conteiner_informacaoTarefa.classList.add('informacaoTarefa');
+
+    const checagemTarefa = document.createElement('span');
+    checagemTarefa.classList.add('material-symbols-outlined', 'checkbox');
+    checagemTarefa.textContent = 'radio_button_unchecked';
+
+    const paragrafoTarefa = document.createElement('p');
+    paragrafoTarefa.textContent = textoTarefa;
+
+    const conteiner_botao_deletarTarefa = document.createElement('div');
+
+    const botao_deletarTarefa = document.createElement('span');
+    botao_deletarTarefa.classList.add('material-symbols-outlined', 'botao_deletaTarefa');
+    botao_deletarTarefa.textContent = 'delete';
+
+    conteiner_informacaoTarefa.appendChild(checagemTarefa);
+    conteiner_informacaoTarefa.appendChild(paragrafoTarefa);
+
+    conteiner_tarefaEspecifica.appendChild(conteiner_informacaoTarefa);
+    
+    conteiner_botao_deletarTarefa.appendChild(botao_deletarTarefa);
+    conteiner_tarefaEspecifica.appendChild(conteiner_botao_deletarTarefa);
+
+    conteiner_tarefas.appendChild(conteiner_tarefaEspecifica);
+
+}
+
+function botao_novaTarefa_clicado()
+{
+    const textoTarefa = prompt('o que vamos fazer?');
+
+    if(textoTarefa && textoTarefa.trim() !== '')
+        cria_novaTarefa(textoTarefa);
+}
+
+botao_adicionaTarefa.addEventListener('click', botao_novaTarefa_clicado);
+
+//checagem de tarefas
+
+function tarefaChecada(event)
+{
+    const icone_clicado = event.target;
+    const informacaoTarefa = icone_clicado.parentElement;
+
+    if(event.target.classList.contains('checkbox'))
+    {
+        const textoTarefa = informacaoTarefa.querySelector('p');
+
+        //essa estrutura é meio nojenta, mas serve pra atribuir um booleano caso, nesse exemplo,
+        //o texto do elemento icone_clicado for igual a 'radio_button_checked'
+        const foiChecada = icone_clicado.textContent === 'radio_button_checked';
+
+        if(foiChecada)
+        {
+            icone_clicado.textContent = 'radio_button_unchecked';
+            icone_clicado.classList.remove('checked');
+            textoTarefa.classList.remove('tarefaConcluida');
+        }
+        else
+        {
+            icone_clicado.textContent = 'radio_button_checked';
+            icone_clicado.classList.add('checked');
+            textoTarefa.classList.add('tarefaConcluida');
+        }
+    }
+
+    else if(icone_clicado.classList.contains('botao_deletaTarefa'))
+    {
+        const confirma_deleta = confirm('tem certeza?');
+
+        if(confirma_deleta)
+        {
+            //tarefa para remover vai procurar o elemento mais próximo que tiver a STRING  oassada como parametro
+            //portanto não basta só passar o nome da classe ou id, tem que botar o indicador de acordo ('.' no caso de classe
+            // e '#' no caso de id) pois ele é um seletor de CSS
+            const tarefa_para_remover = icone_clicado.closest('.tarefaEspecifica');
+
+            if(tarefa_para_remover)
+            {
+                tarefa_para_remover.remove();
+            }  
+        }
+    }
+
+}
+
+
+conteiner_tarefas.addEventListener('click', tarefaChecada);
